f188cb3e5309b4f2bcfaf3c62966de0109f2330a
Touched <Touched@users.noreply.github.com>
2014-09-06 17:46:31 +0200

    Gender picker now repeats properly.

--- image.sh
+++ image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+wine grit images/background.png -gzl -gB 4 -mRtfp -mLs -mzl -ftc -o images/background.h
 wine grit images/lucas.png -gzl -gB 4 -gT 0xaddddd -ftc -o images/lucas.h
 wine grit images/dawn.png -gzl -gB 4 -gT 0xaddddd -ftc -o images/dawn.h
 wine grit images/prof.png -gzl -gB 4 -gT 0xaddddd -ftc -o images/prof.h
